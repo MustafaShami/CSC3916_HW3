1c8583dd223d8039921c0b51c962397844041ca1
MustafaShami <mustafa.shami@ucdenver.edu>
2022-04-15 18:21:56 -0600

    WHY IS IT ALLOWING DUPLICATE USERS??

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function getJSONObjectForMovieRequirement(req) {
     return json;
 }
 
-router.post('/signup', function(req, res) {
+router.post('/signup', {unique:true}, function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
